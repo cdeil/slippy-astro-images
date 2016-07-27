024023c2176621e891adf9367e3e77c5c74bdfa0
Christoph Deil <Deil.Christoph@gmail.com>
2016-07-27 17:24:32 +0200

    Update ng2-router

--- ng2-router/src/app/app.component.ts
+++ ng2-router/src/app/app.component.ts
@@ -1,17 +1,44 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import {SelectComponent} from './select';
-
+import {CatalogService} from './data/catalog.service';
+import {Source} from './data/source';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [CatalogService]
 })
 export class AppComponent {
 
+  public sources: Source[];
+  public catalog;
+  public selectedSource: number;
+  public selectedView: string;
 
+  getCatalog() {
+    this.catalog = this.catalogService.getCatalog();
+  }
+
+  onSourceSelectChange(value) {
+    this.selectedSource = value;
+    console.log('AppComponent: selectedSource: ', this.selectedSource)
+  }
+
+  onViewSelectChange(value) {
+    this.selectedView = value;
+    console.log('AppComponent: selectedView: ', this.selectedView)
+    // TODO: issue route navigation command here!
+  }
+
+
+  constructor(private catalogService: CatalogService) { }
+
+  ngOnInit() {
+    this.getCatalog();
+    this.selectedSource = 0;
+  }
 
 }
