86f4d711b3f7032e51da290bd7a5184a03b53311
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-07-27 16:11:25 -0400

    somewhat working but not complete

--- ng2-router/src/app/app.component.ts
+++ ng2-router/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import {ROUTER_DIRECTIVES} from '@angular/router';
 import {SourceComponent} from './source';
 import {ViewComponent} from './view';
@@ -13,12 +13,13 @@ import {Source} from './data/source';
   directives: [ROUTER_DIRECTIVES],
   providers: [CatalogService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
 
   public sources: Source[];
   public catalog;
   public selectedSource;
   public selectedView;
+  public routerLink;
 
   getCatalog() {
     this.catalog = this.catalogService.getCatalog();
@@ -26,12 +27,29 @@ export class AppComponent {
 
   onSourceChange(value) {
     this.selectedSource = value;
+
     console.log("onSourceChange: ", this.selectedSource);
   }
 
   onViewChange(value) {
     this.selectedView = value;
-        console.log("onViewChange: ", this.selectedView);
+
+    if(this.selectedView == "text") {
+      this.routerLink = "/source";
+    }
+    else if(this.selectedView == "map") {
+      this.routerLink = "/view";
+    }
+    else {
+      console.log("error");
+      this.routerLink = "/";
+    }
+
+    console.log("onViewChange: ", this.selectedView);
+  }
+
+  route() {
+    return this.routerLink;
   }
 
   constructor(private catalogService: CatalogService) { }
@@ -40,6 +58,11 @@ export class AppComponent {
     this.getCatalog();
     this.selectedSource = 0;
     this.selectedView = "text";
+    this.routerLink = '/';
+  }
+
+  ngDoCheck() {
+    // this.route(this.selectedSource);
   }
 
 
