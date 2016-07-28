856043bde8a134d12b34f0002a72b4d098f14a3b
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-07-28 12:59:57 -0400

    add aladin-lite-div

--- ng2-router/src/app/view/view.component.ts
+++ ng2-router/src/app/view/view.component.ts
@@ -3,6 +3,9 @@ import { Source } from '../data/source';
 import { CatalogService } from '../data/catalog.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
+declare var A: any;
+declare var $: any;
+
 @Component({
   moduleId: module.id,
   selector: 'app-view',
@@ -18,6 +21,8 @@ export class ViewComponent implements OnInit {
     source: Source;
     private sub: any;
     public id;
+
+    public aladin;
 
     getCatalog() {
           // this.catalog = this.catalogService.getCatalog();
@@ -39,6 +44,8 @@ export class ViewComponent implements OnInit {
         this.id = id;
         this.catalogService.getSource(id).then(source => this.source = source);
       });
+
+      this.aladin = A.aladin('#aladin-lite-div', {survey: "P/DSS2/color", fov:60});
 
     }
 
