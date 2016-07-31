dfac339165745ca4e8d3880a6860eab298ffeae2
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-07-31 15:18:02 -0400

    cleaned up code in app.component.ts

--- ng2-router/src/app/app.component.ts
+++ ng2-router/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, DoCheck, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewChecked } from '@angular/core';
 import {ROUTER_DIRECTIVES, Router, ActivatedRoute, NavigationEnd, PRIMARY_OUTLET} from '@angular/router';
 import {SourceComponent} from './source';
 import {ViewComponent} from './view';
@@ -15,16 +15,14 @@ declare var $: any;
   directives: [ROUTER_DIRECTIVES],
   providers: [CatalogService]
 })
-export class AppComponent implements OnInit, OnDestroy, DoCheck, AfterViewChecked {
+export class AppComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   public sources: Source[];
-  // public source: Source;
 
   // public catalog;
   public selectedSource;
   public selectedView;
   // private sub: any;
-  // private sub2: any;
   public url;
 
   // getCatalog() {
@@ -63,41 +61,20 @@ export class AppComponent implements OnInit, OnDestroy, DoCheck, AfterViewChecke
     this.catalogService.getCatalog()
       .then(sources => this.sources = sources);
 
-    console.log("sources: ", this.catalogService.getCatalog().then(sources => sources));
-
     // this.sub = this.activatedRoute
     //   .params
     //   .subscribe(params => {
     //     this.selectedSource = +params['id'];
     //     console.log('in subscribe: selectedSource= ', this.selectedSource);
-    //     console.log(params['id']);
+
+    //     console.log(params['id']); // <--- This was never working for us to get the url 'id',
+                                      //      so we get it by doing the below instead.
     //
     //   });
 
       this.url = window.location.toString().split('/');
       this.selectedSource = this.url[this.url.length - 1];
       this.selectedView = this.url[this.url.length - 2];
-
-    // this.sub2 = this.router.events.subscribe((event) => {
-    //   console.log(this.router.url);
-    // });
-
-
-    // this.sub2 = this.router.events
-    //   // .filter(event => event instancecof NavigationEnd)
-    //   .map(_ => this.router.routerState)
-    //   .map(state => {
-    //     let route = this.activatedRoute;
-    //     while(state.firstChild(route)) {
-    //       route = state.firstChild(route);
-    //     }
-    //     return route;
-    //   })
-    //   // .filter(route => route.outlet === PRIMARY_OUTLET)
-    //   .mergeMap(route => route.data)
-    //   .subscribe(data => {
-    //     console.log(data['id'])
-    //   });
 
   }
 
@@ -111,13 +88,8 @@ export class AppComponent implements OnInit, OnDestroy, DoCheck, AfterViewChecke
   ngOnDestroy() {
 
     // this.sub.unsubscribe();
-    // this.sub2.unsubscribe();
 
   }
-
-  ngDoCheck() {
-    // this.route(this.selectedSource);
-    }
 
 
 }
